e5d5285b05708c5f40756bbccede109898252b79
yahyatamim <69247506+yahyatamim@users.noreply.github.com>
2021-08-19 00:42:56 +0600

    Add files via upload

---
+++ medium_multiply/__init__.py
@@ -0,0 +1,2 @@
+# This line of code will allow shorter imports
+from medium_multiply.multiplication import Multiplication
---
+++ medium_multiply/multiplication.py
@@ -0,0 +1,30 @@
+class Multiplication:
+    """
+    Instantiate a multiplication operation.
+    Numbers will be multiplied by the given multiplier.
+    
+    :param multiplier: The multiplier.
+    :type multiplier: int
+    """
+    
+    def __init__(self, multiplier):
+        self.multiplier = multiplier
+    
+    def multiply(self, number):
+        """
+        Multiply a given number by the multiplier.
+        
+        :param number: The number to multiply.
+        :type number: int
+    
+        :return: The result of the multiplication.
+        :rtype: int
+        """
+        
+        return number * self.multiplier
+
+# Instantiate a Multiplication object
+multiplication = Multiplication(2)
+
+# Call the multiply method
+print(multiplication.multiply(5))
