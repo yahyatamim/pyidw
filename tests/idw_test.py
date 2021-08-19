be9f81f61b13a1d76f2ea8ae4a2e473ef0632cdd
yahyatamim <yahyatamim0@gmail.com>
2021-08-19 13:47:28 +0600

    update

--- tests/idw_test.py
+++ tests/idw_test.py
@@ -7,13 +7,13 @@ class MultiplicationTestCase(unittest.TestCase):
         self.assertEqual(1, 1) # dummy test class
 
 
-from pyidw.idw import regression_idw_interpolation
+# from pyidw.idw import regression_idw_interpolation
 
-regression_idw_interpolation(
-    input_point_shapefile='Lowest_temperature.shp',
-    input_raster_file='Elevation.tif',
-    extent_shapefile='BGD_adm0.shp',
-    column_name='Tnn_median',
-    power=2, 
-    polynomial_degree=0,
-    search_radious=4)
+#regression_idw_interpolation(
+#    input_point_shapefile='Lowest_temperature.shp',
+#    input_raster_file='Elevation.tif',
+#    extent_shapefile='BGD_adm0.shp',
+#    column_name='Tnn_median',
+#    power=2, 
+#    polynomial_degree=0,
+#    search_radious=4)
